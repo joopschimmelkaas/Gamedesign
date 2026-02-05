c6688057cbbb149e0c8d5e3fd2322fbdda4fac8c
joopschimmelkaas <224829@lekenlinge.nl>
2026-02-05 14:42:09 +0100

    beginscherm ofzo half af

--- beginscherm.py
+++ beginscherm.py
@@ -1,5 +1,8 @@
 import play
 
-play.new_text(words='BRICK BREAKER', font='Microgramma', font_size=30)
+play.new_box(color='blue4', height=600, width=800 )
+play.new_text(words='BRICK BREAKER', color='azure', y=150,  font='Microgramma', font_size=40)
+play.new_box(color='chartreuse4', width=320, height=60, x=0, y=80 )
+
 
 play.start_program()
